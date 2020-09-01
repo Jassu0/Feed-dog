061c876b97729289b5d6bdf20fa96e1039ccb9c3
Jassu0 <67613766+Jassu0@users.noreply.github.com>
2020-08-31 21:04:21 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var dog, happy, hungry, database;
 var foods, foodstock;
+var fedtime, lastfed, feed, addfood, foodobj;
 function preload() {
     happy = loadImage("happy dog.png");
     hungry = loadImage("Dog.png");
@@ -7,27 +8,37 @@ function preload() {
 
 function setup(){
     database = firebase.database();
-    createCanvas(500,500);
+    createCanvas(1000,500);
     dog = createSprite(250,300,150,150);
     dog.addImage(hungry);
     dog.scale = 0.2;
     foodstock = database.ref('Food');
     foodstock.on("value",readstock);
+    foodobj = new Food();
+    feed = createButton("feed");
+    feed.position(700, 95);
+    feed.mousePressed(feeddog);
+    addfood = createButton("add food");
+    addfood.position(800, 95);
+    addfood.mousePressed(addfood);
 }
 
 function draw(){
     background("white");
-    if(keyWentDown(UP_ARROW)) {
-        writestock(foods);
-        dog.addImage(happy);
+    foodobj.display();
+    fedtime = database.ref('feedtime');
+    fedtime.on("value",function(data){
+        lastfed = data.val();
+    })
+    if(lastfed >= 12) {
+        text("Last Feed"+lastfed%12+"pm",350, 30);
+    } else if(lastfed == 0) {
+        text("Last Feed: 12 AM",350, 30);
+    } else {
+        text("Last Feed"+lastfed+"am",350, 30);
     }
-    
     drawSprites();
-    fill (0);
-    text("food remaining"+foods,170,200);
-    textSize(15);
-    text("Press up arrow to feed the dog",130,50);
-}
+    }
 
 function writestock(x){
     if(x <= 0) {
@@ -41,7 +52,21 @@ function writestock(x){
 }
 function readstock(data) {
     foods = data.val();
+    foodobj.updateFoodStock(foods);
 }
 function showError() {
     console.log("could not read database");
+}
+function feeddog() {
+    dog.addImage(happy);
+    foodobj.updateFoodStock(foodobj.getFoodStock()-1);
+    database.ref('/').update({
+        Food: foodobj.getFoodStock(),feedtime: hour()
+    })
+}
+function addFoods() {
+    foods ++ ;
+    database.ref('/').update({
+        Food: foods
+    })
 }
